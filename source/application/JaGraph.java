2034fd96ab4314ed33f1f19854399bd318a08e13
Hugo Leonel <hugoleonel.code@gmail.com>
2024-02-10 04:04:44 -0300

    renderGraph OK!

--- source/application/JaGraph.java
+++ source/application/JaGraph.java
@@ -25,6 +25,20 @@ public class JaGraph {
 	public static void main(String[] args) {
 		
 
+		String dotCommands = "digraph G {\r\n"
+				+ "a -> b;\r\n"
+				+ "c [shape=box];\r\n"
+				+ "a -> c [weight=29,label=\"algum texto\"];\r\n"
+				+ "subgraph anything {\r\n"
+				+ "/* the following affects only x,y,z */\r\n"
+				+ "node [shape=circle];\r\n"
+				+ "a; x; y -> z; y -> z; /* multiple edges */\r\n"
+				+ "}\r\n"
+				+ "}";
+		
+		
+		renderPDFile(dotCommands, "neato", "MaisUmTeste");
+		
 	}
 
 }
--- source/jni/Framework.java
+++ source/jni/Framework.java
@@ -2,6 +2,10 @@ package jni;
 
 public class Framework {
 
+	static {
+		System.loadLibrary("libJ2GVcpp");
+	}
+	
 	public static native void renderGraph(String dotCommands, String engine, String formatFile, String fileName);
 	
 }

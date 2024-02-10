452034f21d3c7d3a6efdee3c0dddf4894e27b8a1
Hugo Leonel <hugoleonel.code@gmail.com>
2024-02-10 13:53:39 -0300

    Import NativeUtils and Export Lib

--- source/application/JaGraph.java
+++ source/application/JaGraph.java
@@ -22,23 +22,23 @@ public class JaGraph {
 	}
 	
 	
-	public static void main(String[] args) {
-		
-
-		String dotCommands = "digraph G {\r\n"
-				+ "a -> b;\r\n"
-				+ "c [shape=box];\r\n"
-				+ "a -> c [weight=29,label=\"algum texto\"];\r\n"
-				+ "subgraph anything {\r\n"
-				+ "/* the following affects only x,y,z */\r\n"
-				+ "node [shape=circle];\r\n"
-				+ "a; x; y -> z; y -> z; /* multiple edges */\r\n"
-				+ "}\r\n"
-				+ "}";
-		
-		
-		renderPDFile(dotCommands, "neato", "MaisUmTeste");
-		
-	}
+//	public static void main(String[] args) {
+//		
+//
+//		String dotCommands = "digraph G {\r\n"
+//				+ "a -> b;\r\n"
+//				+ "c [shape=box];\r\n"
+//				+ "a -> c [weight=29,label=\"algum texto\"];\r\n"
+//				+ "subgraph anything {\r\n"
+//				+ "/* the following affects only x,y,z */\r\n"
+//				+ "node [shape=circle];\r\n"
+//				+ "a; x; y -> z; y -> z; /* multiple edges */\r\n"
+//				+ "}\r\n"
+//				+ "}";
+//		
+//		
+//		renderPDFile(dotCommands, "neato", "MaisUmTeste");
+//		
+//	}
 
 }
--- source/jni/Framework.java
+++ source/jni/Framework.java
@@ -1,9 +1,17 @@
 package jni;
 
+import java.io.IOException;
+
+import utils.NativeUtils;
+
 public class Framework {
 
 	static {
-		System.loadLibrary("libJ2GVcpp");
+		try {
+			NativeUtils.loadLibraryFromJar("/libJ2GVcpp.dll");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public static native void renderGraph(String dotCommands, String engine, String formatFile, String fileName);
